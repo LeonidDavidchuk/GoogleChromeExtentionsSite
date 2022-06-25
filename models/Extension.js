cdbaa99e517ef5b153e65513744ff8abc700bf7b
LeonidDavidchuk <49954415+LeonidDavidchuk@users.noreply.github.com>
2022-06-25 21:50:43 +0300

    UPD
    Add Extensions to site

---
+++ models/Extension.js
@@ -0,0 +1,10 @@
+const {Schema, model} = require('mongoose')
+const schema = new Schema(
+{
+    Name: {type: String, required: true, unique: false},
+    Price: {type: Number, required: true, unique: false},
+    Image: {type: String, required: true, unique: false},
+    Rate: {type: Number, required: true, unique: false}
+})
+
+module.exports = model('Extension', schema)
--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,37 @@
 const {Router} = require('express')
 const router = Router()
+const Extension = require('../models/Extension')
+
 router.get('/',async(req, res)=>{
-    res.render('index', {title: 'Page'})
+    const extensions = await Extension.find({}).lean()
+    res.render('index', 
+    {
+        title: 'Main Page',
+        extensions
+    })
 })
+
+router.get('/admin',async(req, res)=>{
+    res.render('admin', {title: 'Admin Page'})
+})
+
+router.post('/createproduct',async(req, res)=>{
+    const extension = new Extension(
+    {
+        Name: req.body.Name,
+        Price: req.body.Price,
+        Image: req.body.Image,
+        Rate: req.body.Rate
+    }) 
+    await extension.save()
+    res.redirect('/')
+})
+
+
+
+
+
+
+
 module.exports = router
+
