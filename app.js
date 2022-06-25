7cdf6b09a6123e84162c300a5c97861711f3d7fd
LeonidDavidchuk <49954415+LeonidDavidchuk@users.noreply.github.com>
2022-06-25 16:17:14 +0300

    UPD
    Add main page and folders

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const config = require('config')
 const express = require('express')
 const mongoose = require('mongoose')
-const app = express()
+const expresshandlebars = require('express-handlebars')
+const app=express()
 
 const PORT = config.get('port')
+
+const homeRoute = require('./routes/home')
+
+const hbs = expresshandlebars.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(homeRoute)
+
 async function start()
 {
     try 
     {
-        await mongoose.connect(config.get('connectionstring'), {
+        await mongoose.connect(config.get('connectionstring'), 
+        {
             useNewUrlParser:true,
             useUnifiedTopology: true
         })   
@@ -20,4 +38,5 @@ async function start()
         process.exit(1)
     }
 }
+
 start()
