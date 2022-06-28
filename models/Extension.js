2a316964914fd525a94ad54c2c8bd5833f137202
leoniddavidchuk <Lenjadav@outlook.com>
2022-06-29 02:42:05 +0300

    UPD
    Added a click on a card with goods and displaying information about the product. Added validation to the admin panel.

--- models/Extension.js
+++ models/Extension.js
@@ -1,10 +1,15 @@
 const {Schema, model} = require('mongoose')
 const schema = new Schema(
 {
-    Name: {type: String, required: true, unique: false},
-    Price: {type: Number, required: false, unique: false},
-    Image: {type: String, required: true, unique: false},
-    Rate: {type: Number, required: true, unique: false}
+    Name: { type: String, required: true, unique: false, index: true, sparse: true },
+    Price: { type: Number, required: false, unique: false },
+    Image_Logo: { type: String, required: true, unique: false },
+    Image: { type: String, required: true, unique: false },
+    Image_Description1: { type: String, required: true, unique: false },
+    Image_Description2: { type: String, required: true, unique: false },
+    Image_Description3: { type: String, required: true, unique: false },
+    Rate: { type: Number, required: true, unique: false },
+    Description: { type: String, required: true, unique: false }
 })
 
 module.exports = model('Extension', schema)
--- routes/home.js
+++ routes/home.js
@@ -20,14 +20,29 @@ router.post('/createproduct',async(req, res)=>{
     {
         Name: req.body.Name,
         Price: req.body.Price,
+        Description: req.body.Description,
         Image: req.body.Image,
+        Image_Logo: req.body.Image_Logo,
+        Image_Description1: req.body.Image_Description1,
+        Image_Description2: req.body.Image_Description2,
+        Image_Description3: req.body.Image_Description3,
         Rate: req.body.Rate
     }) 
     await extension.save()
     res.redirect('/')
 })
 
+router.get('/extension/:extensionid', async (req, res) => {
+    var extension = await Extension.findById(req.params.extensionid).lean()
 
+    console.log(extension)
+
+    res.render('extension', {
+        title: extension.Title,
+        IsLogin: true,
+        extension
+    })
+})
 
 
 
